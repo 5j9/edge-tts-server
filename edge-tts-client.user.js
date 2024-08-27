91c14b84b85c11ae911d2e16a53e00e1c565f29a
5j9 <5j9@users.noreply.github.com>
2024-08-27 13:27:30 +0330

    Revert "feat: use GM_xmlhttpRequest"
    This reverts commit ea46da8b9ff69a52a747dd96b4b0dd8dd44754e4.
    The xhr method consumes the whole stream first.

--- edge-tts-client.user.js
+++ edge-tts-client.user.js
@@ -2,7 +2,6 @@
 // @name        edge-tts-client
 // @namespace   https://github.com/5j9/edge-tts-server
 // @grant       GM_registerMenuCommand
-// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 function getText() {
   var sel = window.getSelection().toString();
@@ -10,45 +9,16 @@ function getText() {
   return document.querySelector('article,body').innerText;
 }
 
-function onTextSent() {
+async function tts() {
+  await fetch(
+    'http://127.0.0.1:1775/',
+    { 'method': 'post', 'body': document.title + '\n' + getText() }
+  );
   var audio = new Audio();
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/preload#none
   audio.preload = 'none';
-  // https://developer.mozilla.org/en-US/docs/Web/API/MediaSource
-  var mediaSource = new MediaSource();
-  mediaSource.addEventListener("sourceopen", sourceOpen);
-  audio.src = URL.createObjectURL(mediaSource);
-
-  function sourceOpen(e) {
-    const mimeCodec = 'audio/mpeg';
-    const assetURL = 'http://127.0.0.1:1775/';
-    const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-    fetchAB(assetURL, (buf) => {
-      sourceBuffer.addEventListener("updateend", () => {
-        mediaSource.endOfStream();
-        audio.play();
-      });
-      sourceBuffer.appendBuffer(buf);
-    });
-  }
-
-  function fetchAB(url, cb) {
-    GM_xmlhttpRequest({
-      'method': 'get',
-      url,
-      'responseType': 'arraybuffer',
-      'onload': (xhr) => { cb(xhr.response); }
-    });
-  }
-}
-
-async function tts() {
-  GM_xmlhttpRequest({
-    url: 'http://127.0.0.1:1775/',
-    'method': 'post',
-    'data': document.title + '\n' + getText(),
-    'onload': onTextSent
-  });
+  audio.src = 'http://127.0.0.1:1775/';
+  audio.play();
 }
 
 GM_registerMenuCommand('tts', tts)
