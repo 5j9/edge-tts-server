ea46da8b9ff69a52a747dd96b4b0dd8dd44754e4
5j9 <5j9@users.noreply.github.com>
2024-08-27 06:57:24 +0330

    feat: use GM_xmlhttpRequest
    to bypass csp used by some sites.

--- edge-tts-client.user.js
+++ edge-tts-client.user.js
@@ -2,21 +2,51 @@
 // @name        edge-tts-client
 // @namespace   https://github.com/5j9/edge-tts-server
 // @grant       GM_registerMenuCommand
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 function getText() {
   return document.querySelector('article,body').innerText;
 }
 
-async function tts() {
-  await fetch(
-    'http://127.0.0.1:1775/',
-    { 'method': 'post', 'body': document.title + '\n' + getText() }
-  );
+function onTextSent() {
   var audio = new Audio();
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/preload#none
   audio.preload = 'none';
-  audio.src = 'http://127.0.0.1:1775/';
-  audio.play();
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaSource
+  var mediaSource = new MediaSource();
+  mediaSource.addEventListener("sourceopen", sourceOpen);
+  audio.src = URL.createObjectURL(mediaSource);
+
+  function sourceOpen(e) {
+    const mimeCodec = 'audio/mpeg';
+    const assetURL = 'http://127.0.0.1:1775/';
+    const sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+    fetchAB(assetURL, (buf) => {
+      sourceBuffer.addEventListener("updateend", () => {
+        mediaSource.endOfStream();
+        audio.play();
+      });
+      sourceBuffer.appendBuffer(buf);
+    });
+  }
+
+  function fetchAB(url, cb) {
+    GM_xmlhttpRequest({
+      'method': 'get',
+      url,
+      'responseType': 'arraybuffer',
+      'onload': (xhr) => { cb(xhr.response); }
+    });
+  }
+}
+
+async function tts() {
+  GM_xmlhttpRequest({
+    url: 'http://127.0.0.1:1775/',
+    'method': 'post',
+    'data': document.title + '\n' + getText(),
+    'onload': onTextSent
+  });
 }
 
 GM_registerMenuCommand('tts', tts)
