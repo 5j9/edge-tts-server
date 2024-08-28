24faf7ea7f0d43e1785cd73ce60fe7bc491117da
5j9 <5j9@users.noreply.github.com>
2024-08-28 19:59:20 +0330

    feat: implement audio pause and stop

--- edge-tts-client.user.js
+++ edge-tts-client.user.js
@@ -3,22 +3,44 @@
 // @namespace   https://github.com/5j9/edge-tts-server
 // @grant       GM_registerMenuCommand
 // ==/UserScript==
+var audio = new Audio();
+
 function getText() {
   var sel = window.getSelection().toString();
   if (sel) { return sel; }
   return document.querySelector('article,body').innerText;
 }
 
-async function tts() {
+async function play() {
+
+  if (!audio.paused) {
+    audio.pause();
+    return;
+  } else if (audio.currentTime != 0) {
+    audio.play();
+    return;
+  }
+
+
   await fetch(
     'http://127.0.0.1:1775/',
     { 'method': 'post', 'body': document.title + '\n' + getText() }
   );
-  var audio = new Audio();
   // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/preload#none
   audio.preload = 'none';
   audio.src = 'http://127.0.0.1:1775/';
-  audio.play();
+  audio.play().catch((e) => {
+    if (e != 'AbortError: The play() request was interrupted by a call to pause().') {
+      throw e;
+    }
+  });
 }
 
-GM_registerMenuCommand('tts', tts)
+function stop() {
+  audio.pause();
+  audio.currentTime = 0;
+}
+
+
+GM_registerMenuCommand('play', play);
+GM_registerMenuCommand('stop', stop);
