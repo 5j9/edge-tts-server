8e1dc2b9c02020e22ec91456bb7cc600904097c0
5j9 <5j9@users.noreply.github.com>
2025-05-07 18:24:01 +0330

    feat: rm urls from text

--- monitor_clipboard.py
+++ monitor_clipboard.py
@@ -1,3 +1,4 @@
+import re
 from functools import partial
 from multiprocessing.connection import PipeConnection
 from time import strftime
@@ -6,6 +7,8 @@ from PyQt6.QtWidgets import QApplication
 
 qt_app = QApplication([])
 clipboard = qt_app.clipboard()
+
+rm_urls = partial(re.compile(r'https?:\/\/[^\s]*').sub, '')
 
 
 def skip(text: str):
@@ -27,7 +30,7 @@ def on_clipboard_changed(cb_master: PipeConnection):
     text = mime_data.text()
     if skip(text) is True:
         return
-    cb_master.send(text)
+    cb_master.send(rm_urls(text))
 
 
 def run_qt_app(cb_master: PipeConnection):
