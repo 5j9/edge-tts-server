5f79c1a87e5e8d000c876d533217ee712e76e7d6
5j9 <5j9@users.noreply.github.com>
2025-10-17 09:00:35 +0330

    prefetch_audio: add an overall try-except

--- edge_tts_server/engines/edge.py
+++ edge_tts_server/engines/edge.py
@@ -25,30 +25,37 @@ async def set_voice_names():
 
 async def prefetch_audio(in_q: InputQ, out_q: OutputQ):
     """Prefetch audio for all texts in the queue."""
-    while True:
-        text = await in_q.get()
-        is_fa = persian_match(text) is not None
-        voice = fa_voice if is_fa else en_voice
-        short_text = text[:20] + '...'
-        audio_q = AudioQ()
-        await out_q.put((text, is_fa, audio_q))
-        try:
-            for _ in range(3):
-                try:
-                    async for message in Communicate(
-                        text, voice, connect_timeout=5, receive_timeout=20
-                    ).stream():
-                        if message['type'] == 'audio':
-                            await audio_q.put(message['data'])  # type: ignore
-                except (ClientOSError, NoAudioReceived) as e:
-                    logger.debug(f'Retrying Communicate.stream after {e!r}.')
-                    continue
-                logger.info(f'Audio cached for: {short_text}')
-                break
-        except QueueShutDown:
-            logger.debug(f'audio_q QueueShutDown for {short_text}')
-        except Exception as e:
-            logger.error(f'Error prefetching audio for {short_text}: {e!r}')
-        finally:
-            audio_q.shutdown()
-            await in_q.atask_done()
+    try:
+        while True:
+            text = await in_q.get()
+            is_fa = persian_match(text) is not None
+            voice = fa_voice if is_fa else en_voice
+            short_text = text[:20] + '...'
+            audio_q = AudioQ()
+            await out_q.put((text, is_fa, audio_q))
+            try:
+                for _ in range(3):
+                    try:
+                        async for message in Communicate(
+                            text, voice, connect_timeout=5, receive_timeout=20
+                        ).stream():
+                            if message['type'] == 'audio':
+                                await audio_q.put(message['data'])  # type: ignore
+                    except (ClientOSError, NoAudioReceived) as e:
+                        logger.debug(
+                            f'Retrying Communicate.stream after {e!r}.'
+                        )
+                        continue
+                    logger.info(f'Audio cached for: {short_text}')
+                    break
+            except QueueShutDown:
+                logger.debug(f'audio_q QueueShutDown for {short_text}')
+            except Exception as e:
+                logger.error(
+                    f'Error prefetching audio for {short_text}: {e!r}'
+                )
+            finally:
+                audio_q.shutdown()
+                await in_q.atask_done()
+    except Exception:
+        logger.exception('Unexpected Exception')
