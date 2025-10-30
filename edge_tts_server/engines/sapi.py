4f34cb8b4dca7afd904af32aac648f4dff9a5315
5j9 <5j9@users.noreply.github.com>
2025-10-30 11:03:34 +0330

    use SVSFDefault

--- edge_tts_server/engines/sapi.py
+++ edge_tts_server/engines/sapi.py
@@ -8,6 +8,7 @@ from edge_tts_server import AudioQ
 speaker = wincl.Dispatch('SAPI.SpVoice')
 speaker.Rate = 6.0
 # https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ms720892(v=vs.85)
+SVSFDefault = 0
 SVSFlagsAsync = 1
 SVSFPurgeBeforeSpeak = 2
 # The flag value (1 | 2 = 3) clears the queue and starts the new message immediately (asynchronously).
@@ -16,5 +17,4 @@ speak = speaker.Speak
 
 
 async def prefetch_audio(text: str, lang: str, audio_q: AudioQ):
-    """Prefetch audio for all texts in the queue."""
-    speak(text, SVSFlagsAsync)
+    speak(text, SVSFDefault)
