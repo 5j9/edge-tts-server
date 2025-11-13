8ef513b73ab02436b9c3856340cf5c1dce991815
5j9 <5j9@users.noreply.github.com>
2025-11-13 17:39:03 +0330

    sapi_voice_rate = -3.0
    this is to make the rate for persian edge-tts and sapi approximately the
    same. english-only users will probably want to change this as it sounds
    a bit unnatural at this rate.

--- edge_tts_server/config.py
+++ edge_tts_server/config.py
@@ -4,5 +4,6 @@ min_space_ratio = 0.05
 min_text_length = 1
 # Values for the Rate property range from -10 to 10, which represent the slowest and the fastest speaking rates, respectively.
 # https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ms723606(v=vs.85)
-sapi_voice_rate = -2.0
+sapi_voice_rate = -3.0
+# A list of available SAPI voices are logged in the console when the app starts.
 sapi_voice_name = 'Microsoft David Desktop'
